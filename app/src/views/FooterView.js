83ca58d21969abc85d02398c67dc6dc916938a1b
Marco Trognoni <marco.trognoni@commonhelp.it>
2018-01-08 10:54:42 +0100

    first development vue commit

--- app/src/views/FooterView.js
+++
@@ -1,8 +0,0 @@
-define(['backbone.marionette', 'text!templates/footer.html'], function(Marionette, template){
-    'use strict';
-
-    return Marionette.View.extend({
-        template: _.template(template),
-        className: 'footer-container'
-    });
-});
